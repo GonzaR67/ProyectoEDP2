256cc037a36c8bb40d1f446ef4b2b2baddf62a73
RuizxGonzalo <ruizgonzalo1599@gmail.com>
2026-09-09 16:13:48 -0300

    cambios en dron

--- src/main/java/com/mycompany/proyectoie1/Dron.java
+++ src/main/java/com/mycompany/proyectoie1/Dron.java
@@ -2,4 +2,9 @@ package com.mycompany.proyectoie1;
 public class Dron extends VehiculoAutonomo {
     private String autonomia;
     private int alturaMax;
+
+    @Override
+    public double recargar() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
