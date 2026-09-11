8ceb148650396fe6f06a18ffe66c302244f3e5b3
RuizxGonzalo <ruizgonzalo1599@gmail.com>
2026-09-11 20:21:37 -0300

    avance de gonzalo

--- src/main/java/com/mycompany/proyectoie1/RobotTerrestre.java
+++ src/main/java/com/mycompany/proyectoie1/RobotTerrestre.java
@@ -1,5 +1,101 @@
 package com.mycompany.proyectoie1;
-public class RobotTerrestre {
+
+import java.util.Scanner;
+
+public class RobotTerrestre extends VehiculoAutonomo{
     private String tipoTerreno;
-    private int velocidadMax;
+    private float velocidadMax;
+
+    @Override
+    public double recargar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    public RobotTerrestre() {
+        super();
+        this.tipoTerreno = " ";
+        this.velocidadMax = 0;
+    }
+    
+    
+    @Override
+    public void leerDatos(){
+        super.leerDatos();
+        leerTipoTerreno();
+        leerVelocidad();
+    }
+    
+    public void leerTipoTerreno(){
+        Scanner sc = new Scanner(System.in);
+        int b=0;
+        String str= " ";
+        do{
+            System.out.println("Ingrese el tipo de terreno: ");
+            str= sc.nextLine();
+            try{
+                b = validarString(str);
+                
+            }catch(StringVacio  e){
+                System.out.println(e.getMessage());
+            }catch(StringLargo e){
+                System.out.println(e.getMessage());
+            }
+   
+        }while(b!=1);
+        setModelo(str);
+        
+    }
+    
+    
+    public void leerVelocidad(){
+        float num = 0;
+        int b=0;
+        do{
+            System.out.println("Ingrese la velocidad maxima: ");
+            try{
+                num = validarNumFloat();
+                b=1;
+            }catch(NumeroFlotanteExcepcion e){
+                System.out.println(e.getMessage());
+                
+            }catch(NumeroNegativoExcepcion e){
+                System.out.println(e.getMessage());
+            }catch (VelocidadMaximaExcepcion e){
+                System.out.println(e.getMessage());
+            }
+        } while(b!=1);
+        setVelocidadMax(num); 
+
+    }
+    
+    
+    private float validarVelocidad() throws VelocidadMaximaExcepcion{
+        float num = validarNumFloat();
+        
+        if(num >50){
+            throw new VelocidadMaximaExcepcion("la velocidad");
+        }
+    }
+
+    public String getTipoTerreno() {
+        return tipoTerreno;
+    }
+
+    private void setTipoTerreno(String tipoTerreno) {
+        this.tipoTerreno = tipoTerreno;
+    }
+
+    public float getVelocidadMax() {
+        return velocidadMax;
+    }
+
+    private void setVelocidadMax(float velocidadMax) {
+        this.velocidadMax = velocidadMax;
+    }
+    
+    
+    
+    
+    
+    
 }
---
+++ src/main/java/com/mycompany/proyectoie1/VelocidadMaximaExcepcion.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package com.mycompany.proyectoie1;
+
+/**
+ *
+ * @author ruizg
+ */
+public class VelocidadMaximaExcepcion extends Exception {
+
+    /**
+     * Creates a new instance of <code>VelocidadMaximaExcepcion</code> without
+     * detail message.
+     */
+    public VelocidadMaximaExcepcion() {
+    }
+
+    /**
+     * Constructs an instance of <code>VelocidadMaximaExcepcion</code> with the
+     * specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public VelocidadMaximaExcepcion(String msg) {
+        super(msg);
+    }
+}
