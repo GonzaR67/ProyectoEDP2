2c6e3e52eeecdc2d7d267d16e971bc5d65c57d75
4r1el <langueariel581@gmail.com>
2026-09-12 00:54:55 +0000

    cambios en dron

--- src/main/java/com/mycompany/proyectoie1/Dron.java
+++ src/main/java/com/mycompany/proyectoie1/Dron.java
@@ -1,8 +1,80 @@
 package com.mycompany.proyectoie1;
-public class Dron extends VehiculoAutonomo {
-    private String autonomia;
-    private int alturaMax;
+import java.util.Scanner;
 
+public class Dron extends VehiculoAutonomo {
+    private int autonomia;
+    private int alturaMax;
+    Scanner sc=new Scanner(System.in);
+    
+    public Dron(){
+        this.autonomia=0;
+        this.alturaMax=0;
+    }
+    
+    public void leerAuton(){
+        boolean b=false;
+        int num=0;
+        do{
+            System.out.println("Ingrese la autonomia de vuelo (minutos):");
+            num=sc.nextInt();
+            try{
+                num=super.validarPos();
+                b=true;
+            }catch(NumeroValidoExcepcion | NumeroNegativoExcepcion  e){
+                System.out.println(e.getMessage());
+            }
+        }while(!b);
+        setAutonomia(num);
+    }
+    
+    public void leerAltura(){
+        boolean b=false;
+        int num=0;
+        do{
+            System.out.println("Ingrese la altura maxima:");
+            try{
+                num=super.validarPos();
+                b=true;
+            }catch (NumeroValidoExcepcion | NumeroNegativoExcepcion e){
+                System.out.println(e.getMessage());
+            }
+        }while(!b);
+        setAlturaMax(num);
+    }
+    
+    public int getAutonomia() {
+        return autonomia;
+    }
+
+    public void setAutonomia(int autonomia) {
+        this.autonomia = autonomia;
+    }
+
+    public int getAlturaMax() {
+        return alturaMax;
+    }
+
+    public void setAlturaMax(int alturaMax) {
+        this.alturaMax = alturaMax;
+    }
+    
+    @Override
+    public void leerDatos(){
+        super.leerDatos();
+        leerAuton();
+        leerAltura();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ 
+                "Dron{" + 
+                "autonomia="+autonomia+
+                "alturaMax="+alturaMax+'}';
+    }
+    
+    
+    
     @Override
     public double recargar() {
         throw new UnsupportedOperationException("Not supported yet.");
