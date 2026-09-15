b814fb5ab001799b0b26d32fa577cf03c9627d6d
4r1el <langueariel581@gmail.com>
2026-09-15 03:59:31 +0000

    Agregue el nodo generico

---
+++ src/main/java/com/mycompany/proyectoie1/Nodo.java
@@ -0,0 +1,31 @@
+package com.mycompany.proyectoie1;
+public class Nodo<T> {
+    private T dato;
+    private Nodo<T> ps;
+
+    public Nodo(T dato, Nodo<T> ps) {
+        this.dato = dato;
+        this.ps = ps;
+    }
+    
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getPs() {
+        return ps;
+    }
+
+    public void setPs(Nodo<T> ps) {
+        this.ps = ps;
+    }
+    
+    public String mostrar(){
+        return getDato().toString();
+    }
+    
+}
