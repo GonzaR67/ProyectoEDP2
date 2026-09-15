4f40dd75becd0c79f4be58c5277ff6fca3b8c8ba
4r1el <langueariel581@gmail.com>
2026-09-15 03:59:18 +0000

    Agregue la lista enlazada

---
+++ src/main/java/com/mycompany/proyectoie1/LSE.java
@@ -0,0 +1,24 @@
+package com.mycompany.proyectoie1;
+public class LSE<T> {
+    private Nodo<T> list;
+    
+    public LSE(){
+        list=null;
+    }
+    
+    public Nodo<T> inicio(){
+        return list;
+    }
+    
+    public void insertarPri(T r){
+        Nodo<T> x = new Nodo<T>(r,list);
+        if(list!=null){
+            x.setPs(list);
+        }
+        list=x;
+    }
+    
+    public boolean estaVacia(){
+        return list == null;
+    }
+}
